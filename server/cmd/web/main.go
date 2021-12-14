f793de8957730ec2b292ba02d62c9a60d7edce8d
nao-18 <kyahyo17@yahoo.co.jp>
2021-12-14 23:58:55 +0900

    improving our ws endpoint

--- server/cmd/web/main.go
+++ server/cmd/web/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/nao-18/chat-app-with-websocket/internal/handlers"
 )
 
 func main() {
 	mux := routes()
+
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
 
 	log.Println("Starting web server on port 8081")
 
